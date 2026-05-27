6e4a3ee5fd7e602df95cab0155317f352ec02462
vishal <vishal27207@gmail.com>
2026-05-27 16:57:21 +0530

    first commit

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { App } from './app/app';
+import { Customer } from './app/customer/customer';
+
+bootstrapApplication(Customer, appConfig)
+  .catch((err) => console.error(err));
